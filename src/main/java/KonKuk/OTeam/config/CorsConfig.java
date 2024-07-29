df0c615ecaed4a7adcb55fef79307740359e20f8
able08176 <able08176@gmail.com>
2024-07-29 12:31:39 +0900

    CorsConfig.java 추가

---
+++ src/main/java/KonKuk/OTeam/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package KonKuk.OTeam.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://ec2-13-209-12-231.ap-northeast-2.compute.amazonaws.com:8080") // 클라이언트의 URL
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
